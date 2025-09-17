ad2c9539091438099e2528e359dc3a27f9f4e219
LemuelDev <lemueldev30@gmail.com>
2025-09-17 14:15:04 +0800

    revise ui

--- resources/views/player/layout.blade.php
+++ resources/views/player/layout.blade.php
@@ -8,7 +8,7 @@
     <title>Text-Twist: {{auth()->user()->userProfile->firstname}}</title>
     <link rel="icon" type="image/jpeg" href="{{ asset('logo-game.jpg') }}">
     @vite('resources/css/app.css')
-    @vite('resources/scss/bg.scss')
+    {{-- @vite('resources/scss/bg.scss') --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Press+Start+2P&display=swap" rel="stylesheet">

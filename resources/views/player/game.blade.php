84cce4c1d815ca4adf81409daf0c0a0c73efa378
LemuelDev <lemueldev30@gmail.com>
2025-09-19 22:19:45 +0800

    added changes ui

--- resources/views/player/game.blade.php
+++ resources/views/player/game.blade.php
@@ -47,9 +47,9 @@
             <div id="answer-boxes" class="space-y-4 mb-4"></div>
 
             <!-- Letter Selection Box -->
-            <div class="flex justify-center">
-                <div id="letter-box" class=" place-content-center grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 gap-2 mb-4 shadow-lg p-3 rounded-md"></div>
-            </div>
+          <div class="flex justify-center">
+            <div id="letter-box" class="grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 gap-2 mb-4 shadow-lg p-3 rounded-md mx-auto"></div>
+        </div>
 
             <!-- Controls -->
             <div class="mt-4">
